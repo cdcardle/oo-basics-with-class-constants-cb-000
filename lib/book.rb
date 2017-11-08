24874d9ed05c93ea5ebaea63cdef1a881d78b6f0
Chris Cardle <cdcardle@gmail.com>
2017-11-08 19:37:00 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
-  @GENRES = []
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -13,6 +13,6 @@ class Book
 
   def genre=(genre)
     @genre = genre
-    @GENRES << genre
+    GENRES << genre
   end
 end
